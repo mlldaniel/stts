3e312e9226f0c063d4af38a4406c31455ac337d8
danielinspring <45480066+danielinspring@users.noreply.github.com>
2022-02-26 12:54:14 +0900

    initial commit

---
+++ apps/speech_results/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SpeechResultsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.speech_results'
---
+++ apps/speech_results/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.db.models import JSONField
+from config import settings
+
+
+class SpeechResults(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="User")
+    org_filename = models.TextField(verbose_name="Original File name")
+    result = JSONField()
+
+    created_at = models.DateTimeField(verbose_name='Created DateTime', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='Updated DateTime', auto_now=True)
